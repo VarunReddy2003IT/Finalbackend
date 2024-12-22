03890d8de08330e28e895e8f166eb5bf484c664c
Varun Reddy Pareddy <155606317+VarunReddy2003IT@users.noreply.github.com>
2024-12-22 16:13:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ const PORT = 5000;
 app.use(express.json());
 app.use(cors());
 
+const corsOptions = {
+  origin: 'https://gvpcecc.vercel.app/', // replace with your actual Vercel frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
 // Connect to the database
 connectDB();
 
