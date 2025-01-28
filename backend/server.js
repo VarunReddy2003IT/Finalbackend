53b13c185404e6b4f7156966c8c760a9309597d1
varun <varunreddypareddy@gmail.com>
2025-01-28 19:29:15 +0530

    just route

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ connectDB();
 app.use('/api/events', require('./routes/backendevents'));
 app.use('/api/login', require('./routes/backendlogin'));
 app.use('/api/signup', require('./routes/backendsignup'));
+app.use('/api/profile', require('./routes/backendprofile'));
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
