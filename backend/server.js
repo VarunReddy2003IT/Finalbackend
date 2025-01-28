341e2e395f9a75b7e17f7b1b51dce513d60ab99e
varun <varunreddypareddy@gmail.com>
2025-01-28 19:20:37 +0530

    just image

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,9 @@ app.use(cors());
 connectDB();
 
 // Routes
-app.use('/api/events', require('./routes/backendevents')); // Ensure this line exists
+app.use('/api/events', require('./routes/backendevents'));
 app.use('/api/login', require('./routes/backendlogin'));
 app.use('/api/signup', require('./routes/backendsignup'));
-app.use('/api/profile', require('./routes/backendprofile'));
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
